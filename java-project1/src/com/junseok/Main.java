3e3a473ff8afa81988face3e19557be250fabd1c
선준석/웹파트/SP Tek <junseok14@sptek.co.kr>
2017-12-12 15:31:57 +0900

    Hello World !!

---
+++ java-project1/src/com/junseok/Main.java
@@ -0,0 +1,15 @@
+package com.junseok;
+
+/**
+ * Proejct : workspace_onestore_intelliJ
+ * Developer : 9000075
+ * Date : 2017-12-12
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello Junseok");
+
+    }
+}
